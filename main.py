43d8023591716eefe8bd8f1b8fc50d7a23841a1c
Yoann Dufresne <yoann.dufresne0@gmail.com>
2022-02-24 18:58:39 +0100

    contributors ok

--- main.py
+++ main.py
@@ -3,18 +3,26 @@ from sys import stdin
 
 
 def parse():
-    solution = None
+    C, P = [int(x) for x in stdin.readline().strip().split()]
+    prob = Problem(C, P)
 
-    A, B = [int(x) for x in stdin.readline().strip().split()]
+    for c_idx in range(C):
+        name, num_skills = stdin.readline().strip().split()
+        num_skills = int(num_skills)
+        cont = Contributor(name, c_idx)
 
-    for _ in range(A):
-        line = stdin.readline().strip()
+        for _ in range(num_skills):
+            skill, lvl = stdin.readline().strip().split()
+            lvl = int(lvl)
+            cont.skills[skill] = lvl
 
-    return solution
+        prob.contribs.append(cont)
+
+    return prob
 
 
 
 if __name__ == "__main__":
-    print("This is a test")
+    problem = parse()
 
-    parse()
+    print(problem)
--- solution.py
+++ solution.py
@@ -1,4 +1,17 @@
 
+class Contributor:
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.skills = {}
+
+class Problem:
+    def __init__(self, C, P):
+        self.C = C
+        self.P = P
+        self.contribs = []
+        self.projects = []
+
 class Solution:
     def __init__(self):
         pass
